cf5ee5140313143531d41f2107c430da3f26282f
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2014-03-22 19:21:12 +0100

    Clean code | Add shortcode button

--- inc/fields.php
+++ inc/fields.php
@@ -1,7 +1,9 @@
 <?php
+add_action('init', 'gallery_fields', 10);
 
-if(function_exists("register_field_group"))
-{
+function gallery_fields (){
+	if(function_exists("register_field_group"))
+	{
 	register_field_group(array (
 		'id' => 'acf_entrees-galerie',
 		'title' => 'Entrées galerie',
@@ -47,69 +49,12 @@ if(function_exists("register_field_group"))
 			),
 		),
 		'menu_order' => 0,
-	));
+		));
+	}
 }
 
 
 
-/*if(function_exists("register_field_group"))
-{
-	register_field_group(array (
-		'id' => 'acf_projets-champs-additionnels',
-		'title' => 'Projets - champs additionnels',
-		'fields' => array (
-			array (
-				'key' => 'field_5319d116870fc',
-				'label' => 'Client',
-				'name' => 'client',
-				'type' => 'text',
-				'default_value' => '',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'formatting' => 'html',
-				'maxlength' => '',
-			),
-			array (
-				'key' => 'field_5319d164870fd',
-				'label' => 'Date de réalisation',
-				'name' => 'date_de_realisation',
-				'type' => 'date_picker',
-				'instructions' => 'Sélectionner la date',
-				'date_format' => 'yymmdd',
-				'display_format' => 'dd/mm/yy',
-				'first_day' => 1,
-			),
-			array (
-				'key' => 'field_5319d198870fe',
-				'label' => 'URL',
-				'name' => 'url',
-				'type' => 'text',
-				'default_value' => '',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'formatting' => 'html',
-				'maxlength' => '',
-			),
-		),
-		'location' => array (
-			array (
-				array (
-					'param' => 'post_type',
-					'operator' => '==',
-					'value' => 'projet',
-					'order_no' => 0,
-					'group_no' => 0,
-				),
-			),
-		),
-		'options' => array (
-			'position' => 'normal',
-			'layout' => 'no_box',
-			'hide_on_screen' => array (
-			),
-		),
-		'menu_order' => 0,
-	));
-}*/
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 jQuery(document).ready(function(){
 	jQuery('.bxslider').bxSlider({
 		pagerCustom: '#bx-pager',
-		/*maxWidth: 300,*/
-		/*minSlides: 3,
-		maxSlides: 3*/
 	});
 });
